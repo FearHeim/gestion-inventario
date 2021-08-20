f8bf36352d8e65aa43d5e4a622744275800f024e
José Palacio <53056146+FearHeim@users.noreply.github.com>
2021-08-20 00:01:08 -0500

    Crear Header

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function Header() {
+  return (
+    <div className="Header">
+      <header className="header">
+          <img src="" alt="Logo F12 Store"></img>
+          <div><h1> Inicio</h1></div>
+          <div className="div_2"><h2>Administrador</h2></div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
