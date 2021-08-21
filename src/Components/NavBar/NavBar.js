2b1262ea9df6ff4ff9a349deb4255c0448cdc929
José Palacio <53056146+FearHeim@users.noreply.github.com>
2021-08-21 18:35:39 -0500

    Agregar logo

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
+import logo from "../../assets/img/logo.png";
+
 
 function NavBar() {
   return (
     <div className="Header">
       <header className="header">
-        <img src="" alt="Logo F12 Store"></img>
+        <img src={logo} alt="Logo F12 Store" className='logo'></img>
         <div>
           <NavLink
             exact //saber donde estas exactamente
