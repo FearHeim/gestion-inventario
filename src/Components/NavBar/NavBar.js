c3087a6ea941675851ade4172d2a405f295d5504
José Palacio <53056146+FearHeim@users.noreply.github.com>
2021-08-21 18:43:21 -0500

    Agregar ruta al logo

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -3,12 +3,16 @@ import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 import logo from "../../assets/img/logo.png";
 
-
 function NavBar() {
   return (
     <div className="Header">
       <header className="header">
-        <img src={logo} alt="Logo F12 Store" className='logo'></img>
+        <NavLink
+          exact //saber donde estas exactamente
+          to="/" 
+        >
+          <img src={logo} alt="Logo F12 Store" className="logo"></img>
+        </NavLink>
         <div>
           <NavLink
             exact //saber donde estas exactamente
